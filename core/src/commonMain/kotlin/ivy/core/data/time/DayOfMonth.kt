d7086515d28b32bc30e7f4a0057f831bc67b8932
Iliyan Germanov <iliyan.germanov971@gmail.com>
2023-04-06 00:48:08 +0300

    Add KDoc

--- core/src/commonMain/kotlin/ivy/core/data/time/DayOfMonth.kt
+++ core/src/commonMain/kotlin/ivy/core/data/time/DayOfMonth.kt
@@ -3,7 +3,34 @@ package ivy.core.data.time
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
+import ivy.core.data.time.DayOfMonth.Companion.fromInt
 
+/**
+ * Represents a day of the month as an integer value in the range [[1, 31]], ensuring type-safe operations on day values.
+ * Use [fromInt] method to create a valid `DayOfMonth` object in a type-safe manner.
+ *
+ * # Example
+ * ```
+ * val day1 = DayOfMonth(5)
+ * val day2 = DayOfMonth(10)
+ *
+ * // Add two DayOfMonth objects
+ * val daySum = day1 + day2 // Some(DayOfMonth(15))
+ *
+ * // Subtract two DayOfMonth objects
+ * val dayDifference = day2 - day1 // Some(DayOfMonth(5))
+ *
+ * // Invalid examples
+ * val invalidDay1 = DayOfMonth.fromInt(0) // None
+ * val invalidDay2 = DayOfMonth.fromInt(32) // None
+ * ```
+ *
+ * @property value The day of the month as an integer value.
+ *
+ * # Note
+ * It is recommended to use [fromInt] method to create `DayOfMonth` objects, as it ensures type-safety and returns [Option<DayOfMonth>] depending on the validity of the input.
+ * Keep in mind that days outside the range [[1, 31]] are considered invalid.
+ */
 @JvmInline
 value class DayOfMonth private constructor(val value: Int) {
 
