3a2186cb167541ea3b718a41ef5c838a1818c41d
Iliyan Germanov <iliyan.germanov971@gmail.com>
2023-04-14 21:40:59 +0300

    Make the Desktop DB persistent

--- core/src/jvmMain/kotlin/ivy/core/persistence/SQLDelightDriverFactory.kt
+++ core/src/jvmMain/kotlin/ivy/core/persistence/SQLDelightDriverFactory.kt
@@ -3,10 +3,12 @@ package ivy.core.persistence
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import ivy.Database
+import java.io.File
 
 actual class SQLDelightDriverFactory {
     actual fun createDriver(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        val databasePath = File(System.getProperty("java.io.tmpdir"), "IvyWallet.db")
+        val driver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}")
         Database.Schema.create(driver)
         return driver
     }
